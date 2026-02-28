4adab8a42d3110e31cb47f063d488e8261fa6a2a
Goleti Navathej <navathej101@gmail.com>
2026-03-01 01:07:58 +0530

    Implemented JPA data model

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,59 @@
+package com.wellsfargo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private String email;
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private Advisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client() {}
+
+    public Client(String firstName, String lastName, String email,
+                  String phoneNumber, Advisor advisor, Portfolio portfolio) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.advisor = advisor;
+        this.portfolio = portfolio;
+    }
+
+    public Long getClientId() { return clientId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public Advisor getAdvisor() { return advisor; }
+    public void setAdvisor(Advisor advisor) { this.advisor = advisor; }
+
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
